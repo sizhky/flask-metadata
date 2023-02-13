e7f03d0709ddc57f8b10eca6050804fca3933618
sizhky <yyeshr@gmail.com>
2023-02-13 12:40:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,20 +6,25 @@ app = Flask(__name__)
 
 @app.route("/metadata", methods=["POST"])
 def extract_metadata():
+    print("YOLO")
     # Get the uploaded file from the request
     file = request.files["file"]
+    print("YOLO", file)
 
     # Save the uploaded file to the /tmp directory
     save_path = "/tmp/{}".format(file.filename)
     file.save(save_path)
+    print("YOLO", save_path)
 
     # Extract the file's metadata
     file_size = os.path.getsize(save_path)
     human_readable_size = humanize.naturalsize(file_size)
     file_name = file.filename
+    print("YOLO", file_size)
 
     # Remove the saved file
     os.remove(save_path)
+    print("YOLO")
 
     # Return the extracted metadata
     return {
@@ -29,4 +34,4 @@ def extract_metadata():
     }
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
