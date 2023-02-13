ae0788296f22cd6025fc346e92706e440a578ba0
sizhky <yyeshr@gmail.com>
2023-02-13 12:23:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
-from flask import Flask, jsonify
+from flask import Flask, request
 import os
+import humanize
 
 app = Flask(__name__)
 
+@app.route("/metadata", methods=["POST"])
+def extract_metadata():
+    # Get the uploaded file from the request
+    file = request.files["file"]
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    # Save the uploaded file to the /tmp directory
+    save_path = "/tmp/{}".format(file.filename)
+    file.save(save_path)
 
+    # Extract the file's metadata
+    file_size = os.path.getsize(save_path)
+    human_readable_size = humanize.naturalsize(file_size)
+    file_name = file.filename
 
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    # Remove the saved file
+    os.remove(save_path)
+
+    # Return the extracted metadata
+    return {
+        "file_name": file_name,
+        "file_size": human_readable_size,
+        "exact_file_size": file_size
+    }
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
